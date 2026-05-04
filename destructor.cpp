54177f9d9f6ae5c4def0f6c442705ce13af9ce08
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-05-04 19:37:16 +0700

    constructor dan destructor

--- destructor.cpp
+++ destructor.cpp
@@ -14,3 +14,16 @@ public:
     void isiData();
 };
 
+angka::angka(int i){ // Constructor
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka(){ // Destructor
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
